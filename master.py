riscvarchive/riscv-buildbot-infra

commit 8edae36c8f0f7cb4dbaa916177e700a84dc23089

Define steps with a dict to configure ShellCommand, enable warnings

master.py
@@ -78,9 +78,10 @@ class Target:
         return self._branch
 
     def steps(self):
-        return map(lambda step: map(lambda argv: self.replaceall(argv),
-                                    step),
-                   self._steps)
+        mapped_steps = self._steps
+        for step in mapped_steps :
+            step["command"] = map(lambda argv: self.replaceall(argv), step["command"])
+        return mapped_steps
 
     def find_matching_slaves(self, slave_list):
         return slave_list.slaves()
@@ -246,6 +247,7 @@ from buildbot.steps.shell import ShellCommand
 from buildbot.schedulers.timed import Nightly
 from buildbot.status.mail import MailNotifier
 from buildbot.status.web import authz
+from buildbot.status.results import SUCCESS, WARNINGS, FAILURE
 
 c = BuildmasterConfig = {}
 
@@ -279,6 +281,14 @@ for slave in slave_list.slaves():
         )
     )
 
+# This little dictionary is passed to the ShellCommand constructor to tell the
+# factory how it should handle different return codes
+rc_dict = {
+        0 : SUCCESS,
+        2 : WARNINGS,
+        1 : FAILURE
+        }
+
 # builders map directly to targets, but since targets don't have everything
 for project in project_list.projects():
     for target in project.targets():
@@ -289,7 +299,12 @@ for project in project_list.projects():
                      )
                  )
         for step in target.steps():
-            fact.addStep(ShellCommand(command=step,
+            fact.addStep(ShellCommand(command=step['command'],
+                                      name="{0}".format(step['name']) if 'name' in step else 'shell',
+                                      alwaysRun = True if 'alwaysRun' in step else False,
+                                      haltOnFailure = True,
+                                      flunkOnWarnings = True,
+                                      decodeRC = rc_dict,
                                       timeout=(10 * 60 * 60)
                                   )
                      )

commit 0fcba0003581bd32d07ad21fd7471450bc6291c4

Actually support multiple configurations
This is kind of embarassing: since this code changed to support warnings
went in we haven't actually been elaborating our configurations
correctly.  As a result, only one configuration of each step was being
run for each project.

This changes the step mapping code to perform a deep copy, which causes
each target to get a different set of steps.

This has been broken since September.

Fixes: 8edae36 ("Define steps with a dict to configure ShellCommand,
enable warnings")

master.py
@@ -9,6 +9,7 @@ import json
 import os
 import re
 import sys
+import copy
 
 # This class represents a single slave, which just stores some
 # configuration information about that slave.
@@ -78,9 +79,11 @@ class Target:
         return self._branch
 
     def steps(self):
-        mapped_steps = self._steps
-        for step in mapped_steps :
-            step["command"] = map(lambda argv: self.replaceall(argv), step["command"])
+        mapped_steps = []
+        for step in self._steps :
+            mapped_step = copy.deepcopy(step)
+            mapped_step["command"] = map(lambda argv: self.replaceall(argv), step["command"])
+            mapped_steps.append(mapped_step)
         return mapped_steps
 
     def find_matching_slaves(self, slave_list):